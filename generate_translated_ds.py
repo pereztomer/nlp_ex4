fdad32f4cfacc28427ce00b34020e78963a3daff
Tomer <tomer@jubaan.ai>
2023-02-09 15:01:29 +0200

    push to main pc

--- generate_translated_ds.py
+++ generate_translated_ds.py
@@ -5,31 +5,16 @@ from project_evaluate import compute_metrics, read_file
 
 
 def calculate_score(file1_en, file1_de, file2_en, file2_de):
-    for idx, (sen1, sen2) in enumerate(zip(file1_de, file2_de)):
+    for idx, (sen1, sen2) in enumerate(zip(file1_en, file2_en)):
         if sen1.strip().lower() != sen2.strip().lower():
             raise ValueError('Different Sentences')
-    score = compute_metrics(file1_en, file2_en)
+    score = compute_metrics(file1_de, file2_de)
     print(score)
 
 
-def generate_translated_ds():
+def split_ds_to_sen():
+    split_ds = []
     original_ds = load_ds_labeled(file_path='./data/train.labeled')
-    translated_ds = []
-    for idx, val_dict in enumerate(original_ds):
-        en_sen = val_dict['en']
-        translated = GoogleTranslator(source='auto', target='de').translate(en_sen)
-        new_dict = {'en': en_sen, 'gr': translated}
-        translated_ds.append(new_dict)
-        if idx % 250 == 0:
-            print(f'Done {idx}/10000 samples')
-
-    with open("./translation_tests/complete_translation.json", "w") as outfile:
-        outfile.write(json.dumps(translated_ds, indent=4))
-
-
-def generate_sentence_ds():
-    original_ds = load_ds_labeled(file_path='./data/train.labeled')
-    translated_ds = []
     for idx, val_dict in enumerate(original_ds):
         sen_splits = val_dict['en'].split('.')
         for sp in sen_splits:
@@ -41,14 +26,29 @@ def generate_sentence_ds():
                     if sub_sp == '' or len(sub_sp) == 1:
                         continue
                     else:
-                        translated = GoogleTranslator(source='auto', target='de').translate(sub_sp)
-                        new_dict = {'en': sub_sp, 'gr': translated}
-                        translated_ds.append(new_dict)
-    print(len(translated_ds))
+                        split_ds.append(sub_sp)
+
+    print(len(split_ds))
+
+    with open("./translation_tests/splits_ds_english.json", "w") as outfile:
+        outfile.write(json.dumps(split_ds, indent=4))
 
 
-def main():
-    translated_ds = json.load(open('./translation_tests/'))
+def generate_translated_ds():
+    original_ds = load_ds_labeled(file_path='./data/train.labeled')
+    translated_ds = []
+    for idx, val_dict in enumerate(original_ds):
+        en_sen = val_dict['en']
+        translated = GoogleTranslator(source='auto', target='de').translate(en_sen)
+        new_dict = {'en': en_sen, 'gr': translated}
+        translated_ds.append(new_dict)
+
+    with open("./translation_tests/complete_translation.json", "w") as outfile:
+        outfile.write(json.dumps(translated_ds, indent=4))
+
+
+def compare_ds():
+    translated_ds = json.load(open('./translation_tests/complete_translation.json'))
     file2_en, file2_de = [], []
     for value in translated_ds:
         en_sentence = value['en']
@@ -56,9 +56,9 @@ def main():
         file2_en.append(en_sentence)
         file2_de.append(german_sentence)
 
-    file1_en, file1_de = read_file(file_path='./data/val.labeled')
+    file1_en, file1_de = read_file(file_path='./data/train.labeled')
     calculate_score(file1_en, file1_de, file2_en, file2_de)
 
 
 if __name__ == '__main__':
-    generate_translated_ds()
+    split_ds_to_sen()
