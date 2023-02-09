05d9d8d5317919e9e52c2e74a5b79cd82e9ee118
Tomer <tomer@jubaan.ai>
2023-02-09 09:26:19 +0200

    push to main pc

--- generate_translated_ds.py
+++ generate_translated_ds.py
@@ -1,25 +1,62 @@
 from deep_translator import GoogleTranslator
 from load_ds import load_ds_labeled
+import json
+from project_evaluate import compute_metrics, read_file
 
 
-def main():
+def calculate_score(file1_en, file1_de, file2_en, file2_de):
+    for idx, (sen1, sen2) in enumerate(zip(file1_de, file2_de)):
+        if sen1.strip().lower() != sen2.strip().lower():
+            raise ValueError('Different Sentences')
+    score = compute_metrics(file1_en, file2_en)
+    print(score)
+
+
+def generate_translated_ds():
+    original_ds = load_ds_labeled(file_path='./data/train.labeled')
+    translated_ds = []
+    for idx, val_dict in enumerate(original_ds):
+        en_sen = val_dict['en']
+        translated = GoogleTranslator(source='auto', target='de').translate(en_sen)
+        new_dict = {'en': en_sen, 'gr': translated}
+        translated_ds.append(new_dict)
+
+    with open("./translation_tests/complete_translation.json", "w") as outfile:
+        outfile.write(json.dumps(translated_ds, indent=4))
+
+
+def generate_sentence_ds():
     original_ds = load_ds_labeled(file_path='./data/train.labeled')
     translated_ds = []
     for idx, val_dict in enumerate(original_ds):
         sen_splits = val_dict['en'].split('.')
         for sp in sen_splits:
-            if sp == '' or len(sp) ==1:
+            if sp == '' or len(sp) == 1:
                 continue
             else:
                 sub_splits = sp.split('?')
                 for sub_sp in sub_splits:
-                    if sub_sp == '' or len(sub_sp) ==1:
+                    if sub_sp == '' or len(sub_sp) == 1:
                         continue
                     else:
                         translated = GoogleTranslator(source='auto', target='de').translate(sub_sp)
-                        new_dict = {'en': sub_sp, 'gr':translated}
+                        new_dict = {'en': sub_sp, 'gr': translated}
                         translated_ds.append(new_dict)
     print(len(translated_ds))
 
+
+def main():
+    translated_ds = json.load(open('./translation_tests/'))
+    file2_en, file2_de = [], []
+    for value in translated_ds:
+        en_sentence = value['en']
+        german_sentence = value['gr']
+        file2_en.append(en_sentence)
+        file2_de.append(german_sentence)
+
+    file1_en, file1_de = read_file(file_path='./data/val.labeled')
+    calculate_score(file1_en, file1_de, file2_en, file2_de)
+
+
 if __name__ == '__main__':
-    main()
+    generate_translated_ds()
--- inference.py
+++ inference.py
@@ -3,27 +3,30 @@ from load_ds import load_ds_unlabeled
 
 
 def main():
-    new_file_path = 'data/val.labeled_t5-base_128_max_seq_len'
+    new_file_path = 'data/test'
     unlabeled_ds = load_ds_unlabeled(path='./data/val.unlabeled')
-    translator = pipeline("translation", model="./t5-base_128_max_seq_len/checkpoint-53750(best)")
-
+    translator = pipeline("translation", model="./t5-base_128_max_seq_len_different_lr/best_model")
+    sen_to_translate_lst = []
     for idx, val in enumerate(unlabeled_ds):
         sen_to_translate = "translate German to English: "
         for sen in val['gr']:
             sen_to_translate += sen
+        sen_to_translate_lst.append(sen_to_translate)
+        if idx == 20:
+            break
 
-        val['eg'] = translator(sen_to_translate, max_length=420)[0]['translation_text']
+    translations = translator(sen_to_translate_lst, max_length=420)
 
     with open(new_file_path, "w") as new_file:
-        for idx, val in enumerate(unlabeled_ds):
+        for idx, (val, translated_eng_sen) in enumerate(zip(unlabeled_ds, translations)):
             new_file.write('German:\n')
             for val_2 in val['gr']:
                 new_file.write(val_2)
             new_file.write('English:\n')
-            german_sen = val['eg'].split('.')
-            for ger_sen in german_sen:
-                if ger_sen != '':
-                    new_file.write(ger_sen + '.\n')
+            english_split_sen = translated_eng_sen['translation_text'].split('.')
+            for eng_sen in english_split_sen:
+                if eng_sen != '':
+                    new_file.write(eng_sen + '.\n')
             new_file.write('\n')
 
 
