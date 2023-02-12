e79d3e476a9d2133e19f1aa0a7c56f179a96da9f
Tomer <tomer@jubaan.ai>
2023-02-12 14:03:04 +0200

    push to main pc

--- generate_translated_ds.py
+++ generate_translated_ds.py
@@ -67,42 +67,6 @@ def generate_translated_ds():
     with open("./translation_tests/complete_translation_for_split_ds.json", "w") as outfile:
         outfile.write(json.dumps(translated_ds, indent=4))
 
-
-def splits_paragraph(paragraph):
-    sen_splits = paragraph.split('.')
-    split_ds = []
-    for sp in sen_splits:
-        if sp == '' or len(sp) == 1:
-            continue
-        else:
-            sub_splits = sp.split('?')
-            for sub_sp in sub_splits:
-                if sub_sp == '' or len(sub_sp) == 1:
-                    continue
-                else:
-                    split_ds.append(sub_sp)
-    return split_ds
-
-
-def split_ds_to_sen_old():
-    split_ds = []
-    original_ds = load_ds_labeled(file_path='./data/train.labeled')
-    for idx, val_dict in enumerate(original_ds):
-        en_sentences = splits_paragraph(paragraph=val_dict['en'])
-        ger_sentences = splits_paragraph(paragraph=val_dict['gr'])
-        if len(en_sentences) == len(ger_sentences):
-            for en_sen, gr_sen in zip(en_sentences, ger_sentences):
-                temp_dict = {'en': en_sen, 'gr': gr_sen}
-                split_ds.append(temp_dict)
-        else:
-            temp_dict = {'en': val_dict['en'], 'gr': val_dict['gr']}
-            split_ds.append(temp_dict)
-
-    print(len(split_ds))
-    with open("./translation_tests/splits_ds_english_german.json", "w") as outfile:
-        outfile.write(json.dumps(split_ds, indent=4))
-
-
 def compare_ds():
     translated_ds = json.load(open('./translation_tests/complete_translation.json'))
     file2_en, file2_de = [], []
---
+++ split_ds.py
@@ -0,0 +1,46 @@
+from load_ds import load_ds_labeled
+import json
+
+
+def splits_paragraph(paragraph):
+    sen_splits = paragraph.split('.')
+    split_ds = []
+    for sp in sen_splits:
+        if sp == '' or len(sp) == 1:
+            continue
+        else:
+            sub_splits = sp.split('?')
+            for sub_sp in sub_splits:
+                if sub_sp == '' or len(sub_sp) == 1:
+                    continue
+                else:
+                    split_ds.append(sub_sp)
+    return split_ds
+
+
+def split_ds_to_sen_old():
+    split_ds = []
+    original_ds = load_ds_labeled(file_path='./data/train.labeled')
+    for idx, val_dict in enumerate(original_ds):
+        en_sentences = splits_paragraph(paragraph=val_dict['en'])
+        ger_sentences = splits_paragraph(paragraph=val_dict['gr'])
+        if len(en_sentences) == len(ger_sentences):
+            for en_sen, gr_sen in zip(en_sentences, ger_sentences):
+                temp_dict = {'en': en_sen, 'gr': gr_sen}
+                split_ds.append(temp_dict)
+        else:
+            temp_dict = {'en': val_dict['en'], 'gr': val_dict['gr'], 'split_english': en_sentences}
+
+            split_ds.append(temp_dict)
+
+    print(len(split_ds))
+    with open("./data/splits_ds_english_german.json", "w") as outfile:
+        outfile.write(json.dumps(split_ds, indent=4))
+
+
+def main():
+    split_ds_to_sen_old()
+
+
+if __name__ == '__main__':
+    main()
