d4a4f0a3f13fe797de651570f9f15f4905136d19
pereztomer <pereztomer3@gmail.com>
2023-02-12 14:03:21 +0200

    orgenizing

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from project_evaluate import calculate_score
 
 
 def main():
-    calculate_score(file_path1='./data/val.labeled', file_path2='data/test')
+    calculate_score(file_path1='./data/val.labeled',
+                    file_path2='data/val.labeled_t5-base_128_max_seq_len_short_sentences')
 
 
 if __name__ == '__main__':
